31a170e41f17f414ea57cf09f7eeb249c6866dbe
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-03-16 00:34:02 +0530

    verify code

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,72 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export const POST = async (req: Request) => {
+  await dbConnect();
+  try {
+    const { username, code } = await req.json();
+    const decodedUsrname = decodeURIComponent(username);
+    const user = await UserModel.findOne({ username: decodedUsrname });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const isCodeValid = user?.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+
+      return Response.json(
+        {
+          success: true,
+          message: "Email verified successfully",
+        },
+        {
+          status: 200,
+        }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message: "Verification code expired",
+        },
+        {
+          status: 401,
+        }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Invalid verification code",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+  } catch (error) {
+    console.error("error", error);
+    return Response.json(
+      {
+        success: false,
+        message: "An error occurred while sending the message",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
